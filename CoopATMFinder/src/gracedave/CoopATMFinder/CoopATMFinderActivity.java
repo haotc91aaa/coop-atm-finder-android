41a64da405a7d7943197b771a94f9ba492b43856
Grace DiCinti <grace@graces-macbook.(none)>
2012-06-02 22:08:57 -0400

    Initial commit

---
+++ CoopATMFinder/src/gracedave/CoopATMFinder/CoopATMFinderActivity.java
@@ -0,0 +1,13 @@
+package gracedave.CoopATMFinder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CoopATMFinderActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
